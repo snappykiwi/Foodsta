844ad3ba22a98d57f7e887b4619212cf29726da5
Em Coraccio <emcoraccio@gmail.com>
2019-11-12 21:28:07 -0500

    added browse pg, removed register page/login input

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,23 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material-module';
 import { ProfileComponent } from './profile/profile.component';
+import { BrowseComponent } from './browse/browse.component';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent,
-    ProfileComponent
+    ProfileComponent,
+    BrowseComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
