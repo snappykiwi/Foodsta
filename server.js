ad76f9cf86e5cdb8d39d9043005ed2ce69e1f180
Ibrahim Yunus <younis.ib@gmail.com>
2019-11-16 11:43:15 -0500

    gitignore changes

---
+++ server.js
@@ -0,0 +1,23 @@
+// Get dependencies
+const
+    express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    router = require("./server/routes"),
+    PORT = process.env.PORT || 3000,
+    app = express();
+
+app
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({ extended: false }))
+    .use(express.static(path.join(__dirname, 'dist')))
+    .use(router);
+
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'src/index.html')));
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+app.listen(PORT, () => console.log(`API running on localhost:${PORT}`));
