d8e096a7fba40e0d37adcd098dd788a70d4ca555
Em Coraccio <emcoraccio@gmail.com>
2019-11-19 16:23:40 -0500

    photo cont w/ masonry images on browse & profile pgs

--- client/src/app/photo-container/photo-container.component.ts
+++ client/src/app/photo-container/photo-container.component.ts
@@ -29,5 +29,18 @@ export class PhotoContainerComponent {
     })
   );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  masonryImages = [
+    { image: "https://images.unsplash.com/photo-1505253716362-afaea1d3d1af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80", label: "Greek Salad" },
+    { image: "https://images.unsplash.com/photo-1532980400857-e8d9d275d858?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", label: "Blueberry Pancakes with Oranges" },
+    { image: "https://images.unsplash.com/photo-1506354666786-959d6d497f1a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80", label: "Pizza" },
+    { image: "https://images.unsplash.com/photo-1499028344343-cd173ffc68a9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80", label: "Burger" },
+    { image: "https://images.unsplash.com/photo-1476718406336-bb5a9690ee2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80", label: "Soup" },
+    { image: "https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=987&q=80", label: "French Toast" },
+    { image: "https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80", label: "Steak" },
+    { image: "https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=953&q=80", label: "Breakfast" },
+    { image: "https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80", label: "Pesto Pasta" }
+
+  ]
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
 }
