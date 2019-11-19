09953048b53b459eb99fdc372458c3f8f17d2c94
Em Coraccio <emcoraccio@gmail.com>
2019-11-19 17:58:31 -0500

    added popover to post photos to show more info

--- client/src/app/photo-container/photo-container.component.ts
+++ client/src/app/photo-container/photo-container.component.ts
@@ -29,16 +29,27 @@ export class PhotoContainerComponent {
     })
   );
 
+  name = "Emily"
+
   masonryImages = [
-    { image: "https://images.unsplash.com/photo-1505253716362-afaea1d3d1af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80", label: "Greek Salad" },
-    { image: "https://images.unsplash.com/photo-1532980400857-e8d9d275d858?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", label: "Blueberry Pancakes with Oranges" },
-    { image: "https://images.unsplash.com/photo-1506354666786-959d6d497f1a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80", label: "Pizza" },
-    { image: "https://images.unsplash.com/photo-1499028344343-cd173ffc68a9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80", label: "Burger" },
-    { image: "https://images.unsplash.com/photo-1476718406336-bb5a9690ee2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80", label: "Soup" },
-    { image: "https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=987&q=80", label: "French Toast" },
-    { image: "https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80", label: "Steak" },
-    { image: "https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=953&q=80", label: "Breakfast" },
-    { image: "https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80", label: "Pesto Pasta" }
+    { image: "https://images.unsplash.com/photo-1505253716362-afaea1d3d1af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80", label: "Greek Salad", rating: "7", restaurant: "Giovanni's", link: "http://www.fiveguys.com/menu" },
+    { image: "https://images.unsplash.com/photo-1532980400857-e8d9d275d858?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", label: "Blueberry Pancakes with Oranges", rating: "8", restaurant: "IHOP", link: "http://www.fiveguys.com/menu" },
+    { image: "https://images.unsplash.com/photo-1506354666786-959d6d497f1a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80", label: "Pizza", rating: "9", restaurant: "Sal's Pizza" },
+    { image: "https://images.unsplash.com/photo-1499028344343-cd173ffc68a9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80", label: "Burger", rating: "10", restaurant: "Burger King", link: "http://www.fiveguys.com/menu"},
+    { image: "https://images.unsplash.com/photo-1476718406336-bb5a9690ee2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80", label: "Soup", rating: "9", restaurant: "Idk", link: "http://www.fiveguys.com/menu" },
+    { image: "https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=987&q=80", label: "French Toast", rating: "8", restaurant: "Idk", link: "http://www.fiveguys.com/menu" },
+    { image: "https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80", label: "Steak", rating: "7", restaurant: "Idk", link: "http://www.fiveguys.com/menu" },
+    { image: "https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=953&q=80", label: "Breakfast", rating: "6", restaurant: "Idk", link: "http://www.fiveguys.com/menu" },
+    { image: "https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80", label: "Pesto Pasta", rating: "9", restaurant: "Idk", link: "http://www.fiveguys.com/menu" },
+    { image: "https://images.unsplash.com/photo-1505253716362-afaea1d3d1af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80", label: "Greek Salad", rating: "7", restaurant: "Idk", link: "http://www.fiveguys.com/menu" },
+    { image: "https://images.unsplash.com/photo-1532980400857-e8d9d275d858?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", label: "Blueberry Pancakes with Oranges", rating: "8", restaurant: "Idk", link: "http://www.fiveguys.com/menu" },
+    { image: "https://images.unsplash.com/photo-1506354666786-959d6d497f1a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80", label: "Pizza", rating: "7", restaurant: "Idk", link: "http://www.fiveguys.com/menu" },
+    { image: "https://images.unsplash.com/photo-1499028344343-cd173ffc68a9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80", label: "Burger", rating: "6", restaurant: "Idk", link: "http://www.fiveguys.com/menu" },
+    { image: "https://images.unsplash.com/photo-1476718406336-bb5a9690ee2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80", label: "Soup", rating: "8", restaurant: "Idk", link: "http://www.fiveguys.com/menu" },
+    { image: "https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=987&q=80", label: "French Toast", rating: "2", restaurant: "Idk", link: "http://www.fiveguys.com/menu" },
+    { image: "https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80", label: "Steak", rating: "8", restaurant: "Idk", link: "http://www.fiveguys.com/menu" },
+    { image: "https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=953&q=80", label: "Breakfast", rating: "7", restaurant: "Idk", link: "http://www.fiveguys.com/menu" },
+    { image: "https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80", label: "Pesto Pasta", rating: "7", restaurant: "Idk", link: "http://www.fiveguys.com/menu" }
 
   ]
 
