a05c68dc24636501d9f4412268a6ce81e137fef6
Elias Roussos <eliasisaiah@outlook.com>
2019-11-27 15:10:47 -0500

    -ran build

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const apiRoutes = require('./apiRoutes.js');
+const path = require('path');
 
 
 router.use('/api', apiRoutes);
-router.get('*', (req, res) => res.sendFile(path.join(__dirname, '..', 'client', 'src', 'index.html')));
+console.log("the path I think it is", path.join(__dirname, '..', '..', 'client', 'dist', 'index.html'));
+router.get('/', (req, res) => res.sendFile(path.join(__dirname, '..', '..', 'client', 'dist', 'project-three', 'index.html')));
 
 
 module.exports = router
