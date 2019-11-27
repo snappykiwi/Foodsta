db7832f9cbe56b96e2452db6bda4da991cb6c2ad
Elias Roussos <eliasisaiah@outlook.com>
2019-11-27 16:52:30 -0500

    -removed console.log

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,6 @@ const path = require('path');
 
 
 router.use('/api', apiRoutes);
-console.log("the path I think it is", path.join(__dirname, '..', '..', 'client', 'dist', 'index.html'));
 router.get('/', (req, res) => res.sendFile(path.join(__dirname, '..', '..', 'client', 'dist', 'project-three', 'index.html')));
 
 
