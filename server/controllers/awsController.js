10a5d5ed73796cb2006c806aec64d257a6ddd416
Ibrahim Yunus <younis.ib@gmail.com>
2019-12-15 12:44:58 -0500

    cleaned ApiRoutes, dbController, awsController

--- server/controllers/awsController.js
+++ server/controllers/awsController.js
@@ -1,12 +1,8 @@
-const
-    multer = require('multer'),
-    awsPhotoUpload = require("../awsPhotoUpload");
+const awsPhotoUpload = require("../awsPhotoUpload");
 
 const awsController = {
 
     picUpload: function (req, res) {
-
-        console.log("req.file", req.file);
 
         if (!req.file || Object.keys(req.file).length === 0) {
             return res.status(400).send("No files were uploaded.");
